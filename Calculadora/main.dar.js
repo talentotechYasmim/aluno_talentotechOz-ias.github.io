919d8bfb08ee04a265c61a62734014a817259919
talentotechYasmim <yimyasmim@gmail.com>
2025-01-19 12:16:07 -0300

    Create main.dar.js

---
+++ Calculadora/main.dar.js
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'calculadora.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 81, 4, 212),
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Calculadora'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Calculadora(),
+          ),
+        ],
+      ),
+    );
+  }
+}
